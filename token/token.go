c423d5fa8675c97ffab56d65c506021a62442c1c
Jose Miguel Lozano <jmlozano98@gmail.com>
2023-06-01 22:33:51 +0200

    Defined basic tokens an implemented a simple lexer with tests

--- token/token.go
+++ token/token.go
@@ -1,5 +1,24 @@
 package token
 
+type TokenType string
+
+type Token struct {
+    Type TokenType
+    Literal string
+}
+
+var keywords = map[string]TokenType{
+    "fn": FUNCTION,
+    "let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+    if tok, ok := keywords[ident]; ok {
+        return tok
+    }
+    return IDENT
+}
+
 const (
     ILLEGAL = "ILLEGAL"
     EOF     = "EOF"
