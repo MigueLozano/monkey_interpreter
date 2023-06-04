c7a5cc4e99bdd4dd2dd70e657cc98b605857772b
Jose Miguel Lozano <jmlozano98@gmail.com>
2023-06-04 23:53:56 +0200

    Finished adding basic tokens an their respective lexer implementation

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,6 +1,7 @@
 package lexer
 
 import (
+	"fmt"
 	"monkey/token"
 	"testing"
 )
@@ -13,7 +14,19 @@ func TestNextToken(t *testing.T) {
         };
 
         let result = add(five, ten);
-    `
+
+        !-/*5;
+         5 < 10 > 5;
+
+         if (5 < 10) {
+             return true;
+         } else {
+             return false;
+         }
+
+         10 == 10;
+         10 != 9;
+         `
 
     tests := []struct {
         expectedType token.TokenType
@@ -61,6 +74,48 @@ func TestNextToken(t *testing.T) {
         {token.RPAREN, ")"},
         {token.SEMICOLON, ";"},
 
+        {token.BANG, "!"},
+        {token.MINUS, "-"},
+        {token.SLASH, "/"},
+        {token.ASTERISK, "*"},
+        {token.INT, "5"},
+        {token.SEMICOLON, ";"},
+
+        {token.INT, "5"},
+        {token.LT, "<"},
+        {token.INT, "10"},
+        {token.GT, ">"},
+        {token.INT, "5"},
+        {token.SEMICOLON, ";"},
+
+        {token.IF, "if"},
+        {token.LPAREN, "("},
+        {token.INT, "5"},
+        {token.LT, "<"},
+        {token.INT, "10"},
+        {token.RPAREN, ")"},
+        {token.LBRACE, "{"},
+        {token.RETURN, "return"},
+        {token.TRUE, "true"},
+        {token.SEMICOLON, ";"},
+        {token.RBRACE, "}"},
+        {token.ELSE, "else"},
+        {token.LBRACE, "{"},
+        {token.RETURN, "return"},
+        {token.FALSE, "false"},
+        {token.SEMICOLON, ";"},
+        {token.RBRACE, "}"},
+
+        {token.INT, "10"},
+        {token.EQ, "=="},
+        {token.INT, "10"},
+        {token.SEMICOLON, ";"},
+
+        {token.INT, "10"},
+        {token.NEQ, "!="},
+        {token.INT, "9"},
+        {token.SEMICOLON, ";"},
+
         {token.EOF, ""},
     }
     
@@ -70,6 +125,7 @@ func TestNextToken(t *testing.T) {
         tok := l.NextToken()
         
         if tok.Type != tt.expectedType {
+            fmt.Printf("%q", tok)
             t.Fatalf("tests[%d] - tokentype wrong. Expected=%q, got=%q",
                 i, tt.expectedType, tok.Type)
         }
